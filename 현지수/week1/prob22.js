4b5b076ba008a98b0358687b14b0037f70a0eb3d
Ji-Soo <soo9605@gmail.com>
2020-09-27 11:36:26 +0900

    22

--- 현지수/week1/prob22.js
+++ 현지수/week1/prob22.js
@@ -1,6 +1,51 @@
-const 
+const splitNum = (num) => {
+  if (num.length == 2) {
+    let splits = num.split("");
+    let split1 = Number(splits[0]);
+    let split2 = Number(splits[1]);
+    return [split1, split2];
+  } else if (num.length == 1) {
+    num = "0" + num;
+    let splits = num.split("");
+    let split1 = Number(splits[0]);
+    let split2 = Number(splits[1]);
+    let sum = split1 + split2;
+    return [split1, split2];
+  } else {
+    console.log("0~99사이로 입력하세요.");
+    process.exit(1);
+  }
+};
 
+const countCycle = (num) => {
+  let originNum = num;
+  if (num.length == 2) {
+    originNum = num;
+  } else if (num.length == 1) {
+    originNum = "0" + num;
+  } else {
+    console.log("0~99사이로 입력하세요.");
+    process.exit(1);
+  }
 
+  //let number = Number(num);
+  let cycle = 1;
+
+  while (cycle >= 1) {
+    let splits = splitNum(num);
+    let sum = splits[0] + splits[1];
+    let sumsplits = splitNum(String(sum));
+    let newnum = String(splits[1]) + String(sumsplits[1]);
+
+    if (newnum == originNum) {
+      console.log(cycle);
+      break;
+    } else {
+      cycle += 1;
+      num = newnum;
+    }
+  }
+};
 
 const readline = require("readline");
 
@@ -10,13 +55,7 @@ const rl = readline.createInterface({
 });
 
 rl.question("Write number 0~99.\n ->", function (number) {
-  let splitNum <= 
+  countCycle(number);
 
   rl.close();
 });
-
-
-let number = "22";
-let test = number.split("");
-
-//내일해야지
