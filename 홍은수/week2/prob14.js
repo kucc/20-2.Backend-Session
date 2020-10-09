6ea2a15b4d4faa94ec56f418522bc42981f27d7e
deltaori0 <deltaori0@korea.ac.kr>
2020-10-09 21:32:44 +0900

    solve prob14

--- 홍은수/week2/prob14.js
+++ 홍은수/week2/prob14.js
@@ -1,23 +1,11 @@
-const promise1 = new Promise((res, rej) => {
-  setTimeout(function () {
-    console.log("first promise");
-  }, 6000);
+Promise.all([
+  new Promise((resolve) => setTimeout(() => resolve(1), 7000)),
+  new Promise((resolve) => setTimeout(() => resolve(2), 2000)),
+  new Promise((resolve) => setTimeout(() => resolve(3), 1000)),
+]).then((result) => {
+  console.log(result);
 });
 
-const promise2 = new Promise((res, rej) => {
-  setTimeout(function () {
-    console.log("second promise");
-  }, 2000);
-});
-
-const promise3 = new Promise((res, rej) => {
-  setTimeout(function () {
-    console.log("third promise");
-  }, 1000);
-});
-
-Promise.all([promise1, promise2, promise3])
-  .then(() => {
-    console.log("finished");
-  })
-  .catch((e) => console.log(e.message));
+// Promise.all은 모든 Promise들이 준비될 때까지 기다린다
+// result의 요소는 Promise.all에 전달되는 Promise 순서와 상응한다
+// 첫 번째 Promise가 가장 늦게 이행되지만 처리 결과는 배열의 첫 번째 요소로 저장된다
