5a39cfca196e66af8fdf65dbe18e5f999b7bad1c
EunjeongChoi414 <68999279+giving0414@users.noreply.github.com>
2020-10-31 21:49:22 +0900

    한국어 디코딩 완료

--- 최은정/week04/prob01.js
+++ 최은정/week04/prob01.js
@@ -7,8 +7,8 @@ http
 
     req
       .on("data", (data) => {
-        let food = data.toString().split("=")[0];
-        let price = data.toString().split("=")[1]; //버퍼 디코딩 필요.
+        let food = decodeURIComponent(data.toString().split("=")[0]);
+        let price = decodeURIComponent(data.toString().split("=")[1]);
         menu[food] = price;
       })
       .on("end", () => {
@@ -17,7 +17,7 @@ http
           if (url === "/") {
             res.end(JSON.stringify(menu));
           } else {
-            let reqMenu = url.split("/")[1];
+            let reqMenu = decodeURIComponent(url.split("/")[1]);
             if (menu[reqMenu]) {
               res.end(JSON.stringify(menu[reqMenu]));
             } else {
@@ -37,7 +37,6 @@ http
         } else if (method === "PUT") {
           res.writeHead(200);
           if (url) {
-            //파싱에서 작업해야되나 아님 여기서 해야되나?
             console.log(menu);
             res.end("The price is changed!");
           } else {
@@ -47,7 +46,8 @@ http
         } else if (method === "DELETE") {
           res.writeHead(200);
           if (url) {
-            let deleteMenu = url.split("/")[1];
+            let deleteMenu = decodeURIComponent(url.split("/")[1]);
+            console.log(deleteMenu);
             delete menu[deleteMenu];
             console.log(menu);
             res.end("The menu is deleted");
