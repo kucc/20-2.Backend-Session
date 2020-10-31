7cfbb4e1400d52bf8f01f63f8c0e549b3678452a
EunjeongChoi414 <68999279+giving0414@users.noreply.github.com>
2020-10-31 12:16:12 +0900

    찌끔 수정

--- 최은정/week04/socket-server.js
+++ 최은정/week04/socket-server.js
@@ -6,10 +6,8 @@ const server = net.createServer((socket) => {
     console.log(data.toString());
     pool.push(data.toString());
     console.log(pool);
-    for (let i = 0; i < pool.length; i++) {
-      pool[i];
-    }
-    socket.write(data.toString()); //다른 모든 클라이언트들에게 뿌려주기. pool 에 있는.
+    socket.write(data.toString());
+    //다른 모든 클라이언트들에게 뿌려주기. pool 에 있는.
   });
 });
 
