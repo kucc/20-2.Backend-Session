14adb82ec9a128da44c3ca47360777f87031d01a
EunjeongChoi414 <68999279+giving0414@users.noreply.github.com>
2020-11-08 13:15:04 +0900

    일차수정

--- 최은정/week05/src/route/customer.js
+++ 최은정/week05/src/route/customer.js
@@ -1,4 +1,4 @@
-//배열초기값설정?
+const fs = require("fs");
 const express = require("express");
 const customerList = require("../data/customer.json");
 const customerRouter = express.Router();
@@ -7,63 +7,88 @@ let customers = customerList.customers;
 customerRouter.get("/:year/:month/:date", (req, res, next) => {
   const { year, month, date } = req.params;
   const lastVisit = year + "-" + month + "-" + date;
-  let customersOnthatDay = [];
 
-  for (let c of customers) {
-    if (c.lastVisit === lastVisit) {
-      let customerOnThatDay = {
+  const customersOnthatDay = customers
+    .filter((c) => c.lastVisit === lastVisit)
+    .map((c) => {
+      return {
         name: c.name,
         spending: c.spending,
       };
-      customersOnthatDay.push(customerOnThatDay);
-    }
-  }
+    });
+
   res.json(customersOnthatDay);
 });
 
 customerRouter.get("/", (req, res, next) => {
-  let customersInfo = [];
-  for (let c of customers) {
-    let { id, name } = c;
-    let customerInfo = {
-      id: id,
-      name: name,
+  const allCustomers = customers.map((c) => {
+    return {
+      id: c.id,
+      name: c.name,
     };
-    customersInfo.push(customerInfo);
-  }
-  res.json(customersInfo);
+  });
+
+  res.json(allCustomers);
 });
 
 customerRouter.get("/:id", (req, res, next) => {
   const id = Number(req.params.id);
-  let customersInfo = [];
-  for (let c of customers) {
-    if (c.id === id) {
-      let customerInfo = {
+
+  const idMatchedCustomer = customers
+    .filter((c) => {
+      return c.id === id;
+    })
+    .map((c) => {
+      return {
         name: c.name,
         spending: c.spending,
       };
-      customersInfo.push(customerInfo);
-    }
-  }
-  res.json(customersInfo);
+    });
+
+  res.json(idMatchedCustomer);
 });
 
 customerRouter.post("/", (req, res, next) => {
   const { name, spending } = req.body;
+
   const myDate = new Date();
   const year = myDate.getFullYear().toString();
   const month = (myDate.getMonth() + 1).toString();
   const date = myDate.getDate().toString();
   const today = year + "-" + month + "-" + date;
 
-  for (let c of customers) {
-    if (c.name === name) {
-      c.spending = spending;
-      c.lastVisit = today;
+  const checkCustomers = customers.filter((c) => {
+    return c.name === name;
+  });
+
+  if (checkCustomers.length === 0) {
+    customers.push({
+      id: customers.length + 1,
+      name: name,
+      spending: spending,
+      lastVisit: today,
+    });
+    //file에 넣기
+    fs.writeFile(
+      "./src/data/customer.json",
+      JSON.stringify(customers),
+      (err) => {
+        if (err) {
+          res.send("There was an error while updating the data.");
+        }
+        console.log("WriteFile didn't work");
+      }
+    );
+  } else {
+    for (let c of customers) {
+      if (c.name === name) {
+        c.spending = spending;
+        c.lastVisit = today;
+      }
     }
+    //file에 넣기
   }
-  //기존손님이 아니면?
+
   console.log(customers);
   res.send("Customer data is successfully updated 👍");
 });
